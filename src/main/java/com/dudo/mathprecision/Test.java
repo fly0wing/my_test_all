def967d37691fcf3a371c73e9019019baac24326
zhangkai <fly0wing@126.com>
2014-08-18 10:55:03 +0800

    test

---
+++ src/main/java/com/dudo/mathprecision/Test.java
@@ -0,0 +1,10 @@
+package com.dudo.mathprecision;
+
+/**
+ * Created by zhangkai on 2014/8/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(7*0.8);
+    }
+}
