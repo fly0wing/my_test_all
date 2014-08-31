db05ccfa0eb58a0a507958330f682332bde49f54
zkai <fly0wing@126.com>
2014-08-31 21:54:21 +0800

    s

---
+++ src/main/java/com/dudo/t/A.java
@@ -0,0 +1,7 @@
+package com.dudo.t;
+
+/**
+ * Created by zhangkai on 2014/8/20.
+ */
+ class A {
+}
---
+++ src/main/java/com/dudo/t/a/B.java
@@ -0,0 +1,7 @@
+package com.dudo.t.a;
+
+/**
+ * Created by zhangkai on 2014/8/20.
+ */
+public class B {
+}
