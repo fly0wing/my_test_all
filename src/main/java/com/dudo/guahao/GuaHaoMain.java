05d8d1f6daa94665c2e1e2059e8317029b2a418d
zk <fly0wing@126.com>
2015-06-03 10:00:07 +0800

    添加自动 挂号 程序

--- src/main/java/com/dudo/guahao/GuaHaoMain.java
+++ src/main/java/com/dudo/guahao/GuaHaoMain.java
@@ -24,7 +24,7 @@ public class GuaHaoMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        Cons.isSend = false;//fixme 测试 不发送提交请求.
-
+        Cons.cookie = "Hm_lvt_65f844e6a6e140ab52d02690ed38a38b=1432289098,1432603123,1433208208,1433294346; __c_7V56479HytR4Vcai97V56479HytR4V184vT7azl6=308e2b3c6eedd06119b421d453807fa1; Hm_lpvt_65f844e6a6e140ab52d02690ed38a38b=1433294372";
         // 北医三院, 运动骨科 信息.
         int delay = 7;
         String prefix = "http://www.bjguahao.gov.cn/comm/ghao.php?hpid=142&keid=1150101&date1=";
